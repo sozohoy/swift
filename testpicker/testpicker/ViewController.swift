6752bdfe2ae4c365119413630905cc346bbeee87
석지한 <49385546+sozohoy@users.noreply.github.com>
2022-01-17 18:55:19 +0900

    pickerViewTest

---
+++ testpicker/testpicker/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  testpicker
+//
+//  Created by 한지석 on 2022/01/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    @IBOutlet weak var test: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
+
